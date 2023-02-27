4a2a61e69bca901e1ff8ab40a023d6f2daf0f334
KCmean <kunalmanu2468@gmail.com>
2023-02-27 12:31:25 +0530

    routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { Products, Navbar, Cart } from "./components";
 import { commerce } from "./assets/lib/commerce";
-import { CartElement } from "@stripe/react-stripe-js";
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
 
-  
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
-    
+
     setProducts(data);
   };
-  
+
   const fetchCart = async () => {
     setCart(await commerce.cart.retrieve());
   };
-  
+
   const handleAddToCart = async (productId, quantity) => {
     const item = await commerce.cart.add(productId, quantity);
     setCart(item);
     // console.log(item)
   };
-  
-  useEffect(() => { 
+
+  useEffect(() => {
     fetchProducts();
     fetchCart();
   }, []);
   // console.log( {cart} )
 
   return (
-    <div>
-      <Navbar totalItems={cart.total_items} />
-      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
-      <Cart cart={cart} />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Routes>
+          <Route exact path="/"
+            element = {<Products products={products} onAddToCart={handleAddToCart} />} />
+          <Route exact path="/cart"
+            element = {<Cart cart={cart} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
