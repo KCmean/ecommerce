eae7a71a544dfa38d8265fb949730cee4f6fa92b
KCmean <kunalmanu2468@gmail.com>
2023-03-19 07:55:36 +0530

    feat: add cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core'
+import makeStyles from './styles.js';
+import CartItem from './Cartitem/CartItem';
+import { Link } from 'react-router-dom';
+
+const Cart = ({ cart }) => {
+    const classes = makeStyles();
+    // console.log(cart.line_items.length);
+    // if(cart)console.log(cart.line_items?.length);
+
+    const EmptyCart = () => {
+        return (
+            <Typography variant='subtitle1'> You have no item in your shopping cart, start adding some
+                <Link to="/" className={classes.link}>Start adding some ...</Link>!
+            </Typography>
+        )
+    };
+    const FilledCart = () => {
+        return (
+        <>
+            <Grid container spacing={3}>
+                {cart.line_items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        {/* <div>{item.name}</div> */}
+                        <CartItem item = {item}/>
+                    </Grid>
+                ))}
+            </Grid>
+            <div className={classes.cardDetails}>
+                <Typography variant='h4'>
+                    Subtotal: {cart.subtotal.formatted_with_symbol}
+                </Typography>
+                <div>
+                    <Button className={classes.emptyButton} size="large" type='button' variant='contained' color='secondary'>Empty Cart</Button>
+                    <Button className={classes.checkoutButton} size="large" type='button' variant='contained' color='primary'>Check out</Button>
+                </div>
+            </div>
+        </>
+        )
+    }
+    if(!cart.line_items) return <div>Loading...</div>;
+    return (
+        <Container>
+            <div className={classes.toolbar} />
+            <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
+            {!cart.line_items.length ?
+                <EmptyCart /> 
+                : 
+                <FilledCart />    
+                }
+
+        </Container>
+    )
+}
+
+export default Cart;
