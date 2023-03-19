c5c38538917704d9678bb86fb8f9bdfa4cd7c0fa
KCmean <kunalmanu2468@gmail.com>
2023-03-19 09:37:53 +0530

    feat: added more functionality to cart component

--- src/App.js
+++ src/App.js
@@ -22,22 +22,53 @@ const App = () => {
     setCart(item);
     // console.log(item)
   };
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  };
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+
+    setCart(cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+
+    setCart(cart);
+  };
 
   useEffect(() => {
     fetchProducts();
     fetchCart();
   }, []);
-  // console.log( {cart} )
 
   return (
     <Router>
       <div>
-        <Navbar totalItems={cart.total_items} />
+        <Navbar totalItems={cart?.total_items} />
         <Routes>
-          <Route exact path="/"
-            element = {<Products products={products} onAddToCart={handleAddToCart} />} />
-          <Route exact path="/cart"
-            element = {<Cart cart={cart} />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <Products products={products} onAddToCart={handleAddToCart} />
+            }
+          />
+          <Route
+            exact
+            path="/cart"
+            element={
+              <Cart
+                cart={cart}
+                handleUpdateCartQty={handleUpdateCartQty}
+                handleRemoveFromCart={handleRemoveFromCart}
+                handleEmptyCart={handleEmptyCart}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -2,9 +2,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
-  title: {
-    marginTop: '5%',
-  },
+  // title: {
+  //   marginTop: '5%',
+  // },
   emptyButton: {
     minWidth: '150px',
     [theme.breakpoints.down('xs')]: {
